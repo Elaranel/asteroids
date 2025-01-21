833b9f705ab5c764d55bd0813d53c83551d236ea
Elaranel <elaranel@gmail.com>
2025-01-21 11:39:15 +0000

    Ch3, L7

--- main.py
+++ main.py
@@ -38,10 +38,15 @@ def main():
         for obj in updatable:
             obj.update(dt)
 
-        for obj in asteroids:
-            if obj.collides_with(player):
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
                 print("Game Over!")
                 sys.exit()
+        
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
                 
         
         screen.fill("black")
